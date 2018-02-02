252df6ea7fcb38f8862d20e8f24f82b7da163041
zhongyuan <zhongyuan.nlp@gmail.com>
2018-02-02 16:35:49 +0800

    add zytext module

---
+++ zytext.py
@@ -0,0 +1,10 @@
+def get_line_count(file):
+    """
+    get the line count of a file
+    :param file: file path
+    :return: line count of input file
+    """
+    with open(file=file, mode="rb") as f:
+        for i, l in enumerate(f):
+            pass
+        return i+1
